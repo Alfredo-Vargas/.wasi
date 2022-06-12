fda3157078fbaf887c5cf8ca916e12227504cce8
Alfredo Vargas <alfredo.vargas-paredes@outlook.com>
2022-06-12 23:20:56 +0200

    2022-06-12 23:20:56

--- projects/scripts/wasi_push.sh
+++ projects/scripts/wasi_push.sh
@@ -3,8 +3,11 @@
 # first check for connectivity
 # -q silent mode, --spider don't get page just check availability, $? is the return shell code where 0 means it is OK
 wget -q --spider http://google.com
+message=$(date '+%Y-%m-%d %H:%M:%S')
 
 if [ $? -eq 0 ]; then
+  git -C $HOME/.wasi add .
+  git -C $HOME/.wasi commit -m "$message "
   git -C $HOME/.wasi push
 fi
 
