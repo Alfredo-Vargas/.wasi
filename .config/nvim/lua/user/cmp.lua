c07aa66a6e727946a32981026baacff47ccd5e6a
Alfredo-Vargas <alfredo.vargas-paredes@outlook.com>
2022-07-06 16:56:59 +0200

    new entri bash basics

--- .config/nvim/lua/user/cmp.lua
+++ .config/nvim/lua/user/cmp.lua
@@ -93,20 +93,21 @@ cmp.setup {
       -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
       -- NOTE: order matters
       vim_item.menu = ({
-        -- nvim_lsp = "[LSP]",
-        -- nvim_lua = "[Nvim]",
-        -- luasnip = "[Snippet]",
-        -- buffer = "[Buffer]",
-        -- path = "[Path]",
-        -- emoji = "[Emoji]",
+        nvim_lsp = "[LSP]",
+        nvim_lua = "[Nvim]",
+        luasnip = "[Snippet]",
+        buffer = "[Buffer]",
+        path = "[Path]",
+        emoji = "[Emoji]",
+        dap = "[Dap]"
 
-        nvim_lsp = "",
-        nvim_lua = "",
-        luasnip = "",
-        buffer = "",
-        path = "",
-        emoji = "",
-        dap = "",
+        -- nvim_lsp = "",
+        -- nvim_lua = "",
+        -- luasnip = "",
+        -- buffer = "",
+        -- path = "",
+        -- emoji = "",
+        -- dap = "",
       })[entry.source.name]
       return vim_item
     end,
@@ -119,7 +120,7 @@ cmp.setup {
     { name = "path" },
     { name = "emoji" },
     { name = "dap" },
-    { name = "cmp_tabnine" },
+    -- { name = "cmp_tabnine" },
   },
   confirm_opts = {
     behavior = cmp.ConfirmBehavior.Replace,
---
+++ projects/wiki/bash.md
@@ -0,0 +1,43 @@
+# Bash Scripting Notes
+
+## Bash Fundamentals
+- No spaces when declaring variables!
+- `$(command)` : prints the output of the **command** given to the current position
+- `command1; command2; ...` : several commands in a single line are separated by a semicolon
+- `Globs` allows to create list of files. Globs can be used in python scripts by importing in Python the `Glob` module
+- `$?` returns exits status of $0$ if no error occurred 
+
+### IF conditional
+- In a regular script:
+```bash
+if grep "127.0.0.1" /etc/hosts; then
+  echo "Everything ok"
+else
+  echo "ERROR! 127.0.0.1 is not in /etc/hosts"
+fi
+```
+- Check if a string is empty: 
+```bash
+if test -n "$PATH"; then echo "Your path is not empty"; fi`
+```
+```bash
+if [ -n "$PATH" ]; then echo "Your path is not empty"; fi
+```
+
+### While loop
+- Incremental while
+```bash
+n=1
+while [ $n -le 5 ]; do
+  echo "Iteration number $n"
+  ((n+=1))
+done
+```
+
+### For loop
+```bash
+for logfile in /var/log/*log; do
+  echo "Processing: $logfile"
+  cut =d' ' -f5- $logfile | sort | uniq -c | sort -nr | head -5
+done
+```
