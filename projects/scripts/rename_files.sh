3b07c78c1002b352789245757153ddba28834713
Alfredo-Vargas <alfredo.vargas-paredes@outlook.com>
2022-09-06 22:53:05 +0200

    2022-09-06 22:53:05

---
+++ projects/scripts/rename_files.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Renames the file names listed in the source file "original_names.txt"
+# The renaming requires a prefix and a file extension
+
+names_source="original_names.txt"
+prefix="fig"
+extension=".jpg"
+
+counter=1
+while read -r line; do
+	mv "$line" "$prefix$counter$extension"
+	counter=$((counter + 1))
+done <"$names_source"
